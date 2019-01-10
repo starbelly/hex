080344bf89fd26157de63a3f0d44f3554f87688f
Todd Resudek <toddresudek@gmail.com>
2019-01-10 10:04:02 -0700

    WIP validating licenses

--- lib/mix/tasks/hex.publish.ex
+++ lib/mix/tasks/hex.publish.ex
@@ -357,15 +357,7 @@ defmodule Mix.Tasks.Hex.Publish do
     meta = build.meta
     # TODO: TR - validate licenses against SPDX lists.
     # if license is not in SPDX list, raise an error??
-
     licenses = Map.get(build.package, :licenses, [])
-
-    if licenses do
-      Hex.Shell.info("")
-      Hex.Shell.error("Publishing failed")
-      Hex.Shell.info("Please use a license recognized by SPDX")
-      :error
-    end
 
     {tarball, checksum} = Hex.create_tar!(meta, meta.files, :memory)
     dry_run? = Keyword.get(opts, :dry_run, false)
